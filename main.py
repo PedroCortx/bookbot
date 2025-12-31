59f384cb14b961035c45eaa634508fc6fd2d794c
PedroCortx <pedrocortx@gmail.com>
2025-12-31 19:38:09 -0300

    feat: report

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
-from stats import get_book_text, get_num_words, get_char_count
+from stats import get_book_text, get_num_words, get_char_count, sorted_dicts, report
+import sys
 
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     text = get_book_text(filepath)
     num_words = get_num_words(text)
-    c_count = get_char_count(text)
-    print(f"Found {num_words} total words")
-    print(c_count)
+    char_count = get_char_count(text)
+    sorted_list = sorted_dicts(char_count)
+    report(filepath, text, num_words, char_count, sorted_list)
 
 
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,30 @@ def get_char_count(text):
         else:
             char_count[char] = 1
     return char_count
+
+
+def sort_on(d):
+    return d["num"]
+
+
+def sorted_dicts(char_count):
+    sorted_dicts = []
+    for k in char_count:
+        sorted_dicts.append({"char": k, "num": char_count[k]})
+    sorted_dicts.sort(reverse=True, key=sort_on)
+    return sorted_dicts
+
+
+def report(filepath, text, num_words, char_count, sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {filepath}")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+
+    for i in sorted_list:
+        if not i["char"].isalpha():
+            continue
+        print(f"{i['char']}: {i['num']}")
+
+    print("============= END ===============")
